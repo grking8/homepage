3b1ad98ca26064d809d2a9e75453264f54f678d4
family-guy <guy@zorncapital.com>
2019-08-25 12:47:51 +0100

    Refactor

--- deploy-scripts/external-repos.sh
+++ deploy-scripts/external-repos.sh
@@ -7,8 +7,8 @@
 
 set -e
 
-DIR=/root/project/
-. ${DIR}deploy.cfg
+DIR=/root/project
+. ${DIR}/deploy.cfg
 if [ $1 = true ]; then
     apk update
     apk add openssh-client
@@ -18,7 +18,7 @@ SSH_DIR=/root/.ssh
 if [ ! -d $SSH_DIR ]; then
     mkdir $SSH_DIR
 fi
-ssh-keyscan -H $EXTERNAL_GITHOST >> /root/.ssh/known_hosts
+ssh-keyscan -H $EXTERNAL_GITHOST >> ${SSH_DIR}/known_hosts
 cd ..
 git clone $WEBSERVER_REPO_URL
 git clone $DEPLOY_SCRIPTS_REPO_URL
