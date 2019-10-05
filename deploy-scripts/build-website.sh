abd7ecc1589568f0eb4c625af0165ed0a465bf77
family-guy <guy@zorncapital.com>
2019-10-05 17:25:35 +0100

    Update pkg separately

--- deploy-scripts/build-website.sh
+++ deploy-scripts/build-website.sh
@@ -4,7 +4,8 @@ set -xe
 
 mkdir $STATIC_DIR
 gem install bundler
-bundle check --path vendor/bundle || bundle install --full-index --path vendor/bundle 
+bundle check --path vendor/bundle || \
+bundle update html-pipeline && bundle install --path vendor/bundle 
 bundle exec jekyll build
 if [ -d ".well-known/acme-challenge" ]; then
     cp -r ".well-known" $STATIC_DIR
