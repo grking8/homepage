cf707024e9bce6eede94c63e41ac92eca2e69062
family-guy <guy@zorncapital.com>
2019-10-05 16:56:23 +0100

    Update for new template

--- deploy-scripts/build-website.sh
+++ deploy-scripts/build-website.sh
@@ -2,8 +2,10 @@
 
 set -xe
 
-. deploy.cfg
 mkdir $STATIC_DIR
 gem install bundler
 bundle check --path vendor/bundle || bundle install --path vendor/bundle
 bundle exec jekyll build
+if [ -d ".well-known/acme-challenge" ]; then
+    cp -r ".well-known" $STATIC_DIR
+fi
