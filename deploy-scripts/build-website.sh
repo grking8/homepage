1eead4d351273813475e66366cd79228d03b1199
family-guy <guy@zorncapital.com>
2019-10-05 17:08:19 +0100

    Add option

--- deploy-scripts/build-website.sh
+++ deploy-scripts/build-website.sh
@@ -4,7 +4,7 @@ set -xe
 
 mkdir $STATIC_DIR
 gem install bundler
-bundle check --path vendor/bundle || bundle install --path vendor/bundle
+bundle check --path vendor/bundle || bundle install --full-index --path vendor/bundle 
 bundle exec jekyll build
 if [ -d ".well-known/acme-challenge" ]; then
     cp -r ".well-known" $STATIC_DIR
