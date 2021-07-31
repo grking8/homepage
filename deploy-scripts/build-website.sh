cbac32e36806430f3d6930c181c6643ae151a9ce
family-guy <guy@zorncapital.com>
2021-07-31 11:49:27 +0100

    test: ci change

--- deploy-scripts/build-website.sh
+++ deploy-scripts/build-website.sh
@@ -5,7 +5,7 @@ set -xe
 mkdir $STATIC_DIR
 gem install bundler
 bundle config set --local path 'vendor/bundle'
-bundle check || bundle update html-pipeline && bundle install
+bundle check || bundle install
 bundle exec jekyll build
 if [ -d ".well-known/acme-challenge" ]; then
     cp -r ".well-known" $STATIC_DIR
