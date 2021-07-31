25742bd596709a254518bf0cde676ce8230cfe7e
family-guy <guy@zorncapital.com>
2021-07-31 11:06:12 +0100

    ci: --path is deprecated

--- deploy-scripts/build-website.sh
+++ deploy-scripts/build-website.sh
@@ -4,8 +4,8 @@ set -xe
 
 mkdir $STATIC_DIR
 gem install bundler
-bundle check --path vendor/bundle || \
-bundle update html-pipeline && bundle install --path vendor/bundle 
+bundle config set --local path 'vendor/bundle'
+bundle check || bundle update html-pipeline && bundle install
 bundle exec jekyll build
 if [ -d ".well-known/acme-challenge" ]; then
     cp -r ".well-known" $STATIC_DIR
