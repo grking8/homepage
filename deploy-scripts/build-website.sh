620076766d60a43522fd2697f474cd74fd801bbe
family-guy <guy@zorncapital.com>
2021-07-31 12:38:24 +0100

    fix: typo

--- deploy-scripts/build-website.sh
+++ deploy-scripts/build-website.sh
@@ -4,7 +4,7 @@ set -xe
 
 mkdir $STATIC_DIR
 gem install bundler
-gbundle check --path vendor/bundle || \
+bundle check --path vendor/bundle || \
 bundle update html-pipeline && bundle install --path vendor/bundle 
 bundle exec jekyll build
 if [ -d ".well-known/acme-challenge" ]; then
